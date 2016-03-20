ec513ab9c384c5edfb0ee797ecc797fd48208b98
Lillian Karabaic <anomalily@gmail.com>
2016-03-20 09:04:01 -0700

    copy changes

--- copy/summer.php
+++ copy/summer.php
@@ -1,1 +1,1 @@
-<p>Summer in Portland might be one of the West Coast's best kept secrets. Days are long and dry with temperatures hovering around a comfortable 65-75 degrees. The sun doesn't set until 9PM most days, and there's no better time for hikes at nearby Mt. Hood or Multnomah Falls.</p>
+<p>Summer in Portland might be one of the West Coast's best kept secrets. Days are long and dry with temperatures hovering around a comfortable 65-75 degrees. The sun doesn't set until 9PM most days.  Use that extra daylight for some hikes at nearby Mt. Hood or Multnomah Falls.</p>
