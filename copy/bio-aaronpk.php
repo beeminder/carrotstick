ae44a9567fb3adda1780b6d8c00958f00627b025
Aaron Parecki <aaron@parecki.com>
2016-03-20 11:23:22 -0700

    bio fixes

--- copy/bio-aaronpk.php
+++ copy/bio-aaronpk.php
@@ -4,3 +4,4 @@
 </div>
 <div class="content">
  Aaron Parecki is least-known for making <a href="http://www.hedgy.club"> Hedgy</a>, a cute robot that keeps track of his beers and annoys him when he drinks too much. He's also the cofounder of <a href="http://www.indiewebcamp.com">IndieWebCamp</a> and a professional technologist. 
+</div>
--- copy/bio-anomalily.php
+++ copy/bio-anomalily.php
@@ -3,4 +3,5 @@
   <a href="http://anomalily.net/">anomalily.net</a>
 </div>
 <div class="content">
-Lillian has a degree in economics that she utilizes to justify charging herself money when she doesn't get to inbox zero, clean her house, or go to the gym. She is  often the victim of self-created reverse incentives.  </div>
+Lillian has a degree in economics that she utilizes to justify charging herself money when she doesn't get to inbox zero, clean her house, or go to the gym. She is  often the victim of self-created reverse incentives.
+</div>
