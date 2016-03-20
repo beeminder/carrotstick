168a20f463f3420932b59deadc74ee1f75049db8
Lillian Karabaic <anomalily@gmail.com>
2016-03-20 11:12:41 -0700

    Copy Changes

--- copy/bio-aaronpk.php
+++ copy/bio-aaronpk.php
@@ -3,5 +3,4 @@
   <a href="https://aaronparecki.com/">aaronparecki.com</a>
 </div>
 <div class="content">
-  Bio
-</div>
+ Aaron Parecki is least-known for making <a href="http://www.hedgy.club"> Hedgy</a>, a cute robot that keeps track of his beers and annoys him when he drinks too much. He's also the cofounder of <a href="http://www.indiewebcamp.com">IndieWebCamp</a> and a professional technologist. 
--- copy/bio-anomalily.php
+++ copy/bio-anomalily.php
@@ -3,5 +3,4 @@
   <a href="http://anomalily.net/">anomalily.net</a>
 </div>
 <div class="content">
-  Bio
-</div>
+Lillian has a degree in economics that she utilizes to justify charging herself money when she doesn't get to inbox zero, clean her house, or go to the gym. She is  often the victim of self-created reverse incentives.  </div>
