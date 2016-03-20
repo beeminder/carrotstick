a0302b098eaf952d2d299b260511158187df06fc
Aaron Parecki <aaron@parecki.com>
2016-03-20 09:52:50 -0700

    add description from google doc

--- copy/main-description.php
+++ copy/main-description.php
@@ -1,3 +1,10 @@
-<h3 class="ui header">Copy copy copy</h3>
+<h3 class="ui header">What the heck is this?</h3>
 
-<p>Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.</p>
+<p>We're the kind of people who hear about New Year's Resolutions and think "I could do that - but how can I take it one step further?" Where some people rely on willpower, we use creative motivation devices. Carrot/Stick is a place to talk about the strategies - from the ridiculous to sublime - that you use in your own life to reach your goals. You'll hear about fitness trackers and apps, yes, but you'll hear about "akrasia" and people who charge themselves $1000 if they don't get their inbox to zero.</p>
+
+<p>We envision a place for people to gather where you won't be judged or met with uncertain looks when you say you get 
+You don't have to explain the reasoning why, you just get to start having the conversation with other people as excited about this stuff as you are.</p>
+
+<h3 class="ui header">Who Should Come</h3>
+
+<p>Self-hackers, Behavioral Economics Hobbyists, Quantified Self Geeks, Anyone who read <a href="http://www.amazon.com/Nudge-Improving-Decisions-Health-Happiness-ebook/dp/B00A5DCALY">Nudge</a> and promptly thought "I'm going to implement this in my own life", Fans of <a href="http://www.amazon.com/The-Motivation-Hacker-Nick-Winter-ebook/dp/B00C8N4FNK">The Motivation Hacker</a>.</p>
