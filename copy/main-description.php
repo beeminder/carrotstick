cbf9b3e776fc86d5b1199158eb7fe9acaa576f5b
Lillian Karabaic <anomalily@gmail.com>
2016-03-20 10:42:37 -0700

    copy changes with hover

--- copy/main-description.php
+++ copy/main-description.php
@@ -1,10 +1,12 @@
+
+
 <h3 class="ui header">What the heck is this?</h3>
 
-<p>We're the kind of people who hear about New Year's Resolutions and think "I could do that - but how can I take it one step further?" Where some people rely on willpower, we use creative motivation devices. Carrot/Stick is a place to talk about the strategies - from the ridiculous to sublime - that you use in your own life to reach your goals. You'll hear about fitness trackers and apps, yes, but you'll hear about "akrasia" and people who charge themselves $1000 if they don't get their inbox to zero.</p>
+<p>This is a conference for the behavioral economics enthusiast- the person who uses behavioral economics in their every day life. We're the kind of people who hear about New Year's Resolutions and think "I could do that - but how can I take it one step further?" Where some people rely on willpower, we use creative motivation devices. Carrot/Stick is a place to talk about the strategies - from the ridiculous to sublime - that you use in your own life to reach your goals. You'll hear about trackers and apps, yes, but you'll also hear about  <div class="ui icon button" data-title="Akrasia" data-content="(/əˈkreɪzɪə/) the state of mind in which someone acts against their better judgment through weakness of will" data-variation="inverted">Akrasia</div> and playing Pomodora Poker.</p>
 
-<p>We envision a place for people to gather where you won't be judged or met with uncertain looks when you say you get 
-You don't have to explain the reasoning why, you just get to start having the conversation with other people as excited about this stuff as you are.</p>
+<p>We love this stuff, and there was nowhere in meatspace to talk about it. Some conferences are too academic for the hobbyist, others are focused on tracking your life's data, but not using that data to then change your life. We envision a place where you won't be met with uncertain looks when you talk about paying $5000 to the presidential candidate you hate if you don't finish your thesis on time. This is a safe space for those that want to take behavioral economics research into their own life. You don't have to explain the reasoning why, you just get to start having the conversation with other people as excited about this stuff as you are.</p>
 
 <h3 class="ui header">Who Should Come</h3>
 
-<p>Self-hackers, Behavioral Economics Hobbyists, Quantified Self Geeks, Anyone who read <a href="http://www.amazon.com/Nudge-Improving-Decisions-Health-Happiness-ebook/dp/B00A5DCALY">Nudge</a> and promptly thought "I'm going to implement this in my own life", Fans of <a href="http://www.amazon.com/The-Motivation-Hacker-Nick-Winter-ebook/dp/B00C8N4FNK">The Motivation Hacker</a>.</p>
+<p>Self-hackers, Behavioral Economics Enthusiasts, Quantified Self Geeks, Anyone who read <a href="http://www.amazon.com/Nudge-Improving-Decisions-Health-Happiness-ebook/dp/B00A5DCALY">Nudge</a> and promptly thought "I'm going to implement this in my own life", Fans of <a href="http://www.amazon.com/The-Motivation-Hacker-Nick-Winter-ebook/dp/B00C8N4FNK">The Motivation Hacker</a>.</p>
+
