ab9021afe4572b07bb7c564df43efb69315399d7
Daniel Reeves <dreeves@gmail.com>
2016-04-09 17:27:02 -0700

    Update benson-bubblers.php

--- copy/benson-bubblers.php
+++ copy/benson-bubblers.php
@@ -1,1 +1,4 @@
-<p>Behavior incentives run through Portland's blood - or at least our water. The ubiquitous "Benson Bubbler" public drinking fountains were originally installed in 1912 by Simon Benson, who believed fresh available drinking water would deter loggers from drinking beer during lunch.</p>
+<p>
+Behavior incentives run through Portland's blood &mdash; or at least our water. 
+The ubiquitous "Benson Bubbler" public drinking fountains were originally installed in 1912 by Simon Benson, who believed fresh available drinking water would deter loggers from drinking beer during lunch.
+</p>
