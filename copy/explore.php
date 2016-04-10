b6488b338ba422b8407643356aa9731668a6ff0d
Daniel Reeves <dreeves@gmail.com>
2016-04-09 17:34:13 -0700

    Copy tweak

--- copy/explore.php
+++ copy/explore.php
@@ -1,1 +1,5 @@
-<p>If you're going to make good on your commitments to run or bike, Portland is your urban playground with over 400 miles of beautiful bike trails and paths. Or follow through on your resolution to spend more time in nature in the <a href="http://www.travelportland.com/article/forest-park/">world's largest urban rainforest</a> (there's your sticks!)</p>
+<p>
+If you're going to make good on your commitments to run or bike, Portland is your urban playground with over 400 miles of beautiful bike trails and paths. 
+Or follow through on your resolution to spend more time in nature in the 
+<a href="http://www.travelportland.com/article/forest-park/">world's largest urban rainforest</a> (there's your sticks!).
+</p>
